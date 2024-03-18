119b088a5ef45e89a7737d13f284cd28b10bd427
doug <dougbostick923@gmail.com>
2024-03-18 18:55:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try{
+      const response = await axios.get('http://localhost:3001');
+      setData(response.data);
+    } catch{
+      console.log('problem with axios')
+    }
+   
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Recipe GPT
+    <div>{data}</div>
     </div>
   );
 }
