7d7d0e998aa45b8bba7039020f04a30366bbba6b
doug <dougbostick923@gmail.com>
2024-03-25 22:10:11 -0400

    added assistant functionality

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ import Recipe from './Recipe/Recipe';
 
 function App() {
   const [ingredients, setIngedients] = useState([])
-  const [gptResponse, setGptResponse] = useState('')
+  const [gptResponse, setGptResponse] = useState({})
   const [input, setInput] = useState('')
   const [view, setView] = useState(true)
   const [loading, setLoading] = useState(false)
+  const [assistantView, setAssistantView] = useState(false)
+  const [assistantResponse, setAssistantResponse] = useState({})
 
 
+  const fetchAdvice = async () => {
+    setAssistantResponse({})
+    setView(false)
+    setAssistantView(true)
+    setLoading(true)
+    try{
+      const response = await axios.post('http://localhost:3001/gptAssist', {
+        input
+      })
+      console.log('assist response', response.data)
+      setAssistantResponse(response.data)
+    }catch(err){
+      console.log(err)
+    }
+    setLoading(false)
+  }
   
   const fetchRecipe = async () => {
-    setGptResponse('')
+    setGptResponse({})
     setView(false)
     setLoading(true)
     if(!ingredients.length){
-      setGptResponse('')
+      setGptResponse({})
     } else {
       let ingredientList = ''
       for(const item of ingredients){
@@ -27,7 +45,7 @@ function App() {
         const response = await axios.post('http://localhost:3001/gpt', {
          ingredientList
         })
-        console.log('axios res', response)
+        console.log('axios res', response.data)
         setGptResponse(response.data)
       } catch(err){
         console.log(err)
@@ -48,11 +66,25 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setView(true)
-    setIngedients([...ingredients, input])
-    setInput('')
+    if(assistantView){
+    
+      setView(false)
+      fetchAdvice()
+      setInput('')
+    } else {
+      setView(true)
+      setIngedients([...ingredients, input])
+      setInput('')
+    }
+    
   }
 
+  const toggleAssistant = () => {
+    setAssistantView(!assistantView)
+    setView(!view)
+  }
+
+    //need a button for assisnt view / recipe view toggle
 
   const renderIngredients = ingredients.length ? ingredients.map((item) => {
     return(
@@ -69,11 +101,19 @@ function App() {
     return <Recipe recipe={recipe} />
   }): noRecipes
 
+  const renderAssistantResponse = assistantResponse.response ? <p>{assistantResponse.response}</p> : null
+
+  const inputPlaceHolder = assistantView ? 'Ask me a culinary question' : 'What ingredients do you have?'
+
 
   return (
     <div className="App">
       <div className='searchDiv'>
-        <h1>Recipe GPT</h1>
+        <div>
+        <h1>Mis En Place</h1>
+        <button onClick={toggleAssistant} className='assitantToggle'>{assistantView ? 'Culinary Assistant' : 'Recipe Helper'}</button>
+        </div>
+        
         <div className='inputDiv'>
           <form onSubmit={handleSubmit}>
             <input 
@@ -81,7 +121,7 @@ function App() {
             value={input} 
             onChange={(e) => setInput(e.target.value)}
             autoFocus={true} 
-            placeholder='What ingredients do you have?'
+            placeholder={inputPlaceHolder}
             />
             <button type='submit'>add to ingredients</button>
           </form>
@@ -95,13 +135,19 @@ function App() {
       </div>
       
       <div className='listDiv'>
-        {view && <div className='ingredientsDiv'>
+        {!view && assistantView && <div className='assistantResDiv'>
+            <h1>Let's talk food!</h1>
+            <div className='message'>
+             {loading ? 'Asking our LLM friend your question, one moment!' : renderAssistantResponse}
+            </div>
+          </div>}
+        {view && !assistantView &&<div className='ingredientsDiv'>
           <h1>Ingredients</h1>
           <ul className='ingredientList'>
             {renderIngredients}
           </ul>
         </div>}
-        {!view && <div className='recipeDiv'>
+        {!view && !assistantView && <div className='recipeDiv'>
           <h1>Recipes</h1>
           <div className='gptRes'>
             {loading ? <div className='message'>Asking our LLM friend for recipes, one moment!</div> : renderRecipeResponse}   
---
+++ src/Assistant/Assistant.js
@@ -0,0 +1,8 @@
+import './index.scss'
+import {useState} from 'react'
+
+export default function Assistant(){
+    return(
+        <div>Assistant</div>
+    )
+}
